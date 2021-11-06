8240f5b61190987412ecfa9a179a0a1bea0b23c9
Bbshka <bogdan297@mail.ru>
2021-11-06 05:38:14 +0300

    need to keep it for a while

--- bst.cpp
+++ bst.cpp
@@ -1,36 +1,69 @@
 #include <stdio.h>
 
-template<typename T>
-struct BSNode {
-  T val;
-  BSNode *left = nullptr;
-  BSNode *right = nullptr;
-
-  BSNode (T val) : val(val) {}
-  ~BSNode() {}
-
-  void insert(T val) {
-    BSNode *ptr = nullptr;
-    if (val > this->val) ptr = right;
-    else ptr = left;
-    if (ptr == nullptr) ptr = new BSNode(val);
-    else ptr = ptr->insert(val);
-  }
-};
-
-
-template<typename T>
-class BSTree {
+class bst {
 public:
-  BSTree() {}
-  ~BSTree() {}
+  bst() {head = nullptr; }
+  ~bst() {}
+
+  void insert(int _key) {
+    if (head == nullptr) head = new node(_key);
+    else head = head->insert(_key);
+  }
+
+  void print() {
+    head->print(0);
+  }
 
 private:
-  BSNode *root = nullptr;
+  struct node {
+    int key;
+    node *left, *right;
 
+    node(int _key): key(_key) {
+      left = nullptr;
+      right = nullptr;
+    }
+
+    node *insert(int _key) {
+    node *cur = (_key > key ? right : left);
+    if (cur == nullptr) cur = new node(_key);
+    else cur = cur->insert(_key);
+    return this;
+    }
+
+    void print(int shift) {
+      if (left != nullptr) left->print(shift+1);
+      for (int i = 0; i < shift; ++i) printf(" ");
+      printf("%d\n", this->key);
+      if (right != nullptr) right->print(shift+1);
+    }
+  };
+  node *head;
 };
 
-
 int main() {
-
+  bst b1;
+  b1.insert(5);
+  b1.insert(4);
+  b1.insert(6);
+  b1.insert(3);
+  b1.insert(7);
+  b1.insert(2);
+  b1.insert(1);
+  b1.insert(8);
+  b1.insert(0);
+  b1.insert(9);
+  /*for (int i = 0; i < 10; i++) {
+    printf("%d -> %d\n", i, b1,find(i));
+  }*/
+  printf("____________\n");
+  b1.print();
 }
+
+
+
+
+
+
+
+
