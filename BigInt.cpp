8592e0c784029cca13dc84387aceddfde17f9b0f
Bbshka <bogdan297@mail.ru>
2021-12-01 19:27:56 +0300

    Some changes

--- BigInt.cpp
+++ BigInt.cpp
@@ -31,9 +31,9 @@ public:
 			sign = -1;
 			copy = -t;
 		}
-		if (copy > 1000000000) {
-			body.push_back(t % 1000000000);
-			body.push_back(t / 1000000000);
+		if (copy > RADIX) {
+			body.push_back(t % RADIX);
+			body.push_back(t / RADIX);
 			size += 2;
 		}
 		else {
@@ -81,7 +81,8 @@ public:
 
 	bool modbi(const BigInteger& bi1) {
 		if (size < bi1.size) return false;
-		for (uint32_t i = 0; i < size; ++i) {
+		if (size > bi1.size) return true;
+		for (ssize_t i = size - 1; i != -1; --i) {
 			if (body[i] < bi1.body[i]) return false;
 		}
 		return true;
@@ -90,6 +91,36 @@ public:
 	bool check_zero() const {
 		if (size == 1 && body[0] == 0) return true;
 		return false;
+	}
+
+	BigInteger& operator++() {
+		if (sign == -1) {
+			if (size == 1 && body[0] == 1) {
+				sign = 1;
+				body[0] = 0;
+				return *this;
+			}
+			*this += 1;
+			return *this;
+		}
+		if (body[0] % 10 == 9) {
+			uint32_t index = 0;
+			for (; index < size && body[index] == RADIX - 1; ++index) body[index] = 0;
+			if (index == size) {
+				body.push_back(1);
+				++size;
+				return *this;
+			}
+			++body[index];
+			return *this;
+		}
+		++body[0];
+		return *this;
+	}
+
+    BigInteger& operator--() { //Yes^ it's stupid, but i'm tired...
+		*this -= 1;
+		return *this;
 	}
 
 	BigInteger& operator+=(const BigInteger& bi1) { 
@@ -103,22 +134,22 @@ public:
 		uint32_t i = 0;
 		for (; i < min_size; ++i) {
 			tmp = body[i] + bi1.body[i] + CarryFlag;
-			body[i] = tmp % 1000000000;
-			CarryFlag = tmp / 1000000000;
+			body[i] = tmp % RADIX;
+			CarryFlag = tmp / RADIX;
 		}
 		for (; i < size; ++i) {
 			tmp = body[i] + CarryFlag;
-			body[i] = tmp % 1000000000;
-			CarryFlag = tmp / 1000000000;
+			body[i] = tmp % RADIX;
+			CarryFlag = tmp / RADIX;
 		}
 		for (; i < bi1.size; ++i) {
 			tmp = bi1.body[i] + CarryFlag;
-			body.push_back(tmp % 1000000000);
+			body.push_back(tmp % RADIX);
 			++size;
-			CarryFlag = tmp / 1000000000;
+			CarryFlag = tmp / RADIX;
 		}
 		if (CarryFlag) {
-			body.push_back(CarryFlag % 1000000000);
+			body.push_back(CarryFlag % RADIX);
 			++size;
 		}
 		return *this;
@@ -136,20 +167,60 @@ public:
 		uint32_t max_size = (size >= bi1.size ? size : bi1.size);
 		uint32_t tmp, CarryFlag = 0;
 		for (uint32_t index = 0; index < max_size; ++index) {
-			tmp = 1000000000 + (index < copy.size) * copy.body[index % copy.size] - (index < copy_min.size) * copy_min.body[index % copy_min.size] - CarryFlag;
-			copy.body[index] = tmp % 1000000000;
-			CarryFlag = (tmp >= 1000000000 ? 0 : 1);
+			tmp = RADIX + (index < copy.size) * copy.body[index % copy.size] - (index < copy_min.size) * copy_min.body[index % copy_min.size] - CarryFlag;
+			copy.body[index] = tmp % RADIX;
+			CarryFlag = (tmp >= RADIX ? 0 : 1);
 		}
 		*this = copy;
 		if (check_zero()) sign = 1;
+		/*for (ssize_t i = size - 1; body[i] == 0; -- i) {
+			body.pop_back();
+			--size;
+		}*/
 		return *this;
 	}
 
 	BigInteger& operator*=(const BigInteger& bi1) {
+		if (bi1.sign == -1) sign = -sign;
+		BigInteger copy_max = (modbi(bi1) ? *this : bi1);
+		BigInteger copy_min = (modbi(bi1) ? bi1 : *this);
+		BigInteger ans;
+		for (uint32_t index = 0; index < copy_max.size + copy_min.size; ++index) {
+			ans.body.push_back(0);
+			++ans.size;
+		}
+		ans.sign = sign;
+		size_t tmp;
+		uint32_t CarryFlag = 0;
+		for (uint32_t deg = 0; deg < ans.size; ++deg) {
+			tmp = 0;
+			for (uint32_t j = (deg > copy_min.size - 1 ? copy_min.size - 1 : deg), i = deg - j; i < copy_max.size && j < copy_min.size; ++i, --j) {
+				tmp += (static_cast<size_t>(copy_max.body[i]) * static_cast<size_t>(copy_min.body[j]));
+			}
+			tmp += static_cast<size_t>(CarryFlag);
+			ans.body[deg] = tmp % static_cast<size_t>(RADIX);
+			CarryFlag = tmp / static_cast<size_t>(RADIX);
+		}
+		if (CarryFlag != 0) {
+			ans.body.push_back(CarryFlag  % RADIX);
+			++ans.size;
+		}
+		*this = ans;
+		if (check_zero()) sign = 1;
+		return *this;
+	}
 
+	BigInteger operator*(const BigInteger& bi1) const {
+		BigInteger copy = *this;
+		copy *= bi1;
+		return copy;
 	}
 
 	BigInteger& operator/=(const BigInteger& bi1) {
+		BigInteger ans = 0;
+		while(*this - (ans + 1) * bi1 > 0) ans += 1;
+		*this = ans;
+		return *this;
 		
 	}
 
@@ -173,6 +244,7 @@ private:
 	std::vector<uint32_t> body;
 	uint32_t size;
 	int8_t sign;
+	uint32_t RADIX = 1000000000;
 };
 
 void _reverse(char* buf, const int size) {
@@ -228,8 +300,8 @@ std::ostream& operator<<(std::ostream& output, const BigInteger& bi1) {
 	}
 	output << bi1.body[i];
 	for (--i; i != -1; --i) {
-		for (int j = 0, deg = 100000000; j < 9; j++, deg /= 10) {
-			output << ((bi1.body[i] / deg) % 10);
+		for (uint32_t j = 0, deg = bi1.RADIX / 10; j < 9; j++, deg /= static_cast<uint32_t>(10)) {
+			output << ((bi1.body[i] / deg) % static_cast<uint32_t>(10));
 		}
 		//output << bi1.body[i];
 	}
@@ -238,7 +310,7 @@ std::ostream& operator<<(std::ostream& output, const BigInteger& bi1) {
 }
 
 int main() {
-	BigInteger p1, p2, n1, n2;
+	/*BigInteger p1, p2, n1, n2;
 	std::cout << "Enter first positive:\n";
 	std::cin >> p1;
 	std::cout << "Enter second positive:\n";
@@ -252,8 +324,31 @@ int main() {
 	std::cout << "n1 + n2 = " << n1 + n2 << '\n';
 	std::cout << "n1 - n2 = " << n1 - n2 << '\n';
 	std::cout << "p1 + n1 = " << p1 + n1 << '\n';
-	std::cout << "p1 - n1 = " << p1 - n1 << '\n';
+	std::cout << "p1 - n1 = " << p1 - n1 << '\n';*/
 	/*BigInteger a = 999999999;
 	BigInteger b = 1;
 	std::cout << a + b << ' ' << a + b << '\n';*/
+
+	/*BigInteger a;
+	std::cin >> a;
+	--a;
+	std::cout << a << ' ';
+	++a;
+	++a;
+	std::cout << a << ' ';
+	--a;
+	std::cout << a << '\n';*/
+
+	BigInteger a, b;
+	std::cin >> a >> b;
+	std::cout << a << ' ' << b << '\n';
+	std::cout << a * b << '\n';
+	
+	/*int a1, b1;
+	std::cin >> a1 >> b1;
+	BigInteger a = a1, b = b1;
+	a /= b;
+	std::cout << "C++ a / b = " << a1/b1 << '\n';
+	std::cout << "BigInteger a /= b : " << a << '\n';*/
+
 }
