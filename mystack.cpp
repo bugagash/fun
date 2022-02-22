4bfcb4813c67e18ab4aab4921cc124987edf7896
Bbshka <bogdan297@mail.ru>
2022-02-22 21:42:31 +0300

    I forget, but may contain changes

--- mystack.cpp
+++ mystack.cpp
@@ -1,107 +1,89 @@
-#include <stdio.h>
 #include <iostream>
-#define MAXLEN 1000
 
-bool check(char* str1, char* str2);
-
-class stack {
+#define TT printf("%s: line %d\n", __PRETTY_FUNCTION__, __LINE__)
+class stack_ll {
 public:
-  stack() { //constructor
-    head = new node(0); 
-  }
-
-  stack(int val) {
-    head = new node(0);
-    push(val);
-  }
-
-  stack(const stack& st) { //copy_construcor
-    head = new node(0);
-    for (auto p = st.head->prev, q = head;
-         p!= nullptr;
-         p = p->prev, q = q->prev) {
-      q->prev = new node(p->val);
-    }
-  }
-
-  ~stack() {
-    for (auto p = head->prev; p != nullptr; ) {
-      node *tmp = p->prev;
-      delete p;
-      p = tmp;
-    }
-  }
-
-  void push(int val) {
-    node* tmp = new node(val);
-    tmp->prev = head->prev;
-    head->prev = tmp;
-    printf("ok\n");
-  }
-
-  int pop() {
-    node* tmp = head->prev;
-    /*if (tmp == nullptr) {  //Don't make stupid requests!
-      printf("Nothing to be pop\n");
-      return -1;
-    }*/ //For shitass!
-    head->prev = tmp->prev;
-    int v = tmp->val;
-    delete tmp;
-    return v; 
-  }
-
-  int top() {
-    return head->prev->val;
-  }
-
-  int size() {
-    size_t size = 0;
-    for (node* p = head->prev; p!= nullptr; p = p->prev) size++;
-    return size;
-  }
-
-  void print() {
-    for (node* p = head->prev; p != nullptr; p = p->prev) {
-      printf("%d\n", p->val);
-    }
-  }
-
+    using T = int;
+    stack_ll();  // Конструктор, create
+    stack_ll(stack_ll const &st);  // Конструктор копирования
+    stack_ll & operator=(stack_ll const &st); // Оператор копирования
+    ~stack_ll();
+    void push(T val);
+    T pop();
+    T top();
+    bool empty() const;
 private:
-  struct node{
-    int val;
-    node *prev;
-    node (int val): val(val) {
-      this->prev = nullptr;
-    }
-  };
-  node* head;
+    struct node {
+        T val;
+        node *prev;
+        node(T val): val(val), prev(nullptr) {}
+    };
+    node *head;
+    void destroy();
+    void clone(stack_ll const &st);
 };
 
-bool check(char* str1, char* str2) {
-  for (int i = 0; str1[i] && str2[i]; i++) {
-    if (str1[i] != str2[i]) return false;
-  }
-  return true;
+stack_ll::stack_ll() { // Конструктор, create
+    TT;
+    head = new node(0);
 }
 
-int main() {
-  char* buf = new char[MAXLEN];
-  stack s;
-  char pop[4] = {'p', 'o', 'p', '\0'};
-  char top[4] = {'t', 'o', 'p', '\0'};
-  char size[5] = {'s', 'i', 'z', 'e', '\0'};
-  char exit[5] = {'e', 'x', 'i', 't', '\0'};
-  char push[5] = {'p', 'u', 's', 'h', '\0'};
+stack_ll::stack_ll(stack_ll const &st) { // Конструктор копирования
+    TT;
+    clone(st);
+}
 
-  fgets(buf, MAXLEN, stdin);
-  while(!check(buf, exit)) {
-    if (check(buf, top)) printf("%d\n", s.top());
-    else if (check(buf, pop)) printf("%d\n", s.pop());
-    else if (check(buf, push)) s.push(atoi(buf+5));
-    else if (check(buf, size)) printf("%d\n", s.size());
-    fgets(buf, MAXLEN, stdin);
-  }
-  delete[] buf;
-    
+stack_ll & stack_ll::operator=(stack_ll const &st) { // Оператор копирования
+    TT;
+    destroy();
+    clone(st);
+    return *this; 
+}
+
+stack_ll::~stack_ll() {   // Деструктор, destroy
+    TT;
+    destroy();
+}
+
+void stack_ll::push(T val) {
+    TT;
+    node *tmp = new node(val);
+    tmp->prev = head->prev;
+    head->prev = tmp;
+}
+
+stack_ll::T stack_ll::pop() {
+    TT;
+    node *tmp = head->prev;
+    head->prev = tmp->prev;
+    T val = tmp->val;
+    delete tmp;
+    return val;    
+}
+
+stack_ll::T stack_ll::top() {
+    TT;
+    return head->prev->val;
+}
+
+bool stack_ll::empty() const {
+    TT;
+    return head->prev == nullptr;
+}
+
+void stack_ll::destroy() {
+    while (!empty()) {
+        pop();
+    }
+    delete head;
+    head = nullptr;
+}
+
+void stack_ll::clone(stack_ll const &st) {
+    head = new node(0);
+    for (auto p = st.head->prev, q = head; 
+        p != nullptr; 
+        p = p->prev, q = q->prev) {
+        q->prev = new node(p->val);
+    }
 }
